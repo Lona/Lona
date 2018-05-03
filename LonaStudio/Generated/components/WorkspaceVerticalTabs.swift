4efd098bbe846302e91b13156a8f3c8758b59345
Devin Abbott <devinabbott@gmail.com>
2018-05-02 17:47:06 -0700

    Improve workspace vertical tabs

--- LonaStudio/Generated/components/WorkspaceVerticalTabs.swift
+++ LonaStudio/Generated/components/WorkspaceVerticalTabs.swift
@@ -34,13 +34,27 @@ public class WorkspaceVerticalTabs: NSBox {
 
   // MARK: Private
 
+  private var innerView = NSBox()
   private var tabIconLayersView = TabIcon()
   private var tabIconDocumentationView = TabIcon()
+  private var dividerView = NSBox()
 
   private var topPadding: CGFloat = 0
   private var trailingPadding: CGFloat = 0
   private var bottomPadding: CGFloat = 0
   private var leadingPadding: CGFloat = 0
+  private var innerViewTopMargin: CGFloat = 0
+  private var innerViewTrailingMargin: CGFloat = 0
+  private var innerViewBottomMargin: CGFloat = 0
+  private var innerViewLeadingMargin: CGFloat = 0
+  private var innerViewTopPadding: CGFloat = 0
+  private var innerViewTrailingPadding: CGFloat = 0
+  private var innerViewBottomPadding: CGFloat = 0
+  private var innerViewLeadingPadding: CGFloat = 0
+  private var dividerViewTopMargin: CGFloat = 0
+  private var dividerViewTrailingMargin: CGFloat = 0
+  private var dividerViewBottomMargin: CGFloat = 0
+  private var dividerViewLeadingMargin: CGFloat = 0
   private var tabIconLayersViewTopMargin: CGFloat = 0
   private var tabIconLayersViewTrailingMargin: CGFloat = 0
   private var tabIconLayersViewBottomMargin: CGFloat = 0
@@ -50,11 +64,18 @@ public class WorkspaceVerticalTabs: NSBox {
   private var tabIconDocumentationViewBottomMargin: CGFloat = 0
   private var tabIconDocumentationViewLeadingMargin: CGFloat = 0
 
+  private var innerViewLeadingAnchorConstraint: NSLayoutConstraint?
+  private var innerViewTopAnchorConstraint: NSLayoutConstraint?
+  private var innerViewBottomAnchorConstraint: NSLayoutConstraint?
+  private var dividerViewTrailingAnchorConstraint: NSLayoutConstraint?
+  private var dividerViewLeadingAnchorConstraint: NSLayoutConstraint?
+  private var dividerViewTopAnchorConstraint: NSLayoutConstraint?
+  private var dividerViewBottomAnchorConstraint: NSLayoutConstraint?
   private var tabIconLayersViewTopAnchorConstraint: NSLayoutConstraint?
   private var tabIconLayersViewLeadingAnchorConstraint: NSLayoutConstraint?
-  private var tabIconDocumentationViewBottomAnchorConstraint: NSLayoutConstraint?
   private var tabIconDocumentationViewTopAnchorConstraint: NSLayoutConstraint?
   private var tabIconDocumentationViewLeadingAnchorConstraint: NSLayoutConstraint?
+  private var dividerViewWidthAnchorConstraint: NSLayoutConstraint?
   private var tabIconLayersViewHeightAnchorConstraint: NSLayoutConstraint?
   private var tabIconLayersViewWidthAnchorConstraint: NSLayoutConstraint?
   private var tabIconDocumentationViewHeightAnchorConstraint: NSLayoutConstraint?
@@ -64,28 +85,57 @@ public class WorkspaceVerticalTabs: NSBox {
     boxType = .custom
     borderType = .noBorder
     contentViewMargins = .zero
+    innerView.boxType = .custom
+    innerView.borderType = .noBorder
+    innerView.contentViewMargins = .zero
+    dividerView.boxType = .custom
+    dividerView.borderType = .noBorder
+    dividerView.contentViewMargins = .zero
 
-    addSubview(tabIconLayersView)
-    addSubview(tabIconDocumentationView)
+    addSubview(innerView)
+    addSubview(dividerView)
+    innerView.addSubview(tabIconLayersView)
+    innerView.addSubview(tabIconDocumentationView)
 
     tabIconLayersView.icon = #imageLiteral(resourceName: "icon-tab-layers")
     tabIconDocumentationView.icon = #imageLiteral(resourceName: "icon-tab-docs")
+    dividerView.fillColor = Colors.grey300
   }
 
   private func setUpConstraints() {
     translatesAutoresizingMaskIntoConstraints = false
+    innerView.translatesAutoresizingMaskIntoConstraints = false
+    dividerView.translatesAutoresizingMaskIntoConstraints = false
     tabIconLayersView.translatesAutoresizingMaskIntoConstraints = false
     tabIconDocumentationView.translatesAutoresizingMaskIntoConstraints = false
 
+    let innerViewLeadingAnchorConstraint = innerView
+      .leadingAnchor
+      .constraint(equalTo: leadingAnchor, constant: leadingPadding + innerViewLeadingMargin)
+    let innerViewTopAnchorConstraint = innerView
+      .topAnchor
+      .constraint(equalTo: topAnchor, constant: topPadding + innerViewTopMargin)
+    let innerViewBottomAnchorConstraint = innerView
+      .bottomAnchor
+      .constraint(equalTo: bottomAnchor, constant: -(bottomPadding + innerViewBottomMargin))
+    let dividerViewTrailingAnchorConstraint = dividerView
+      .trailingAnchor
+      .constraint(equalTo: trailingAnchor, constant: -(trailingPadding + dividerViewTrailingMargin))
+    let dividerViewLeadingAnchorConstraint = dividerView
+      .leadingAnchor
+      .constraint(equalTo: innerView.trailingAnchor, constant: innerViewTrailingMargin + dividerViewLeadingMargin)
+    let dividerViewTopAnchorConstraint = dividerView
+      .topAnchor
+      .constraint(equalTo: topAnchor, constant: topPadding + dividerViewTopMargin)
+    let dividerViewBottomAnchorConstraint = dividerView
+      .bottomAnchor
+      .constraint(equalTo: bottomAnchor, constant: -(bottomPadding + dividerViewBottomMargin))
     let tabIconLayersViewTopAnchorConstraint = tabIconLayersView
       .topAnchor
-      .constraint(equalTo: topAnchor, constant: topPadding + tabIconLayersViewTopMargin)
+      .constraint(equalTo: innerView.topAnchor, constant: innerViewTopPadding + tabIconLayersViewTopMargin)
     let tabIconLayersViewLeadingAnchorConstraint = tabIconLayersView
       .leadingAnchor
-      .constraint(equalTo: leadingAnchor, constant: leadingPadding + tabIconLayersViewLeadingMargin)
-    let tabIconDocumentationViewBottomAnchorConstraint = tabIconDocumentationView
-      .bottomAnchor
-      .constraint(equalTo: bottomAnchor, constant: -(bottomPadding + tabIconDocumentationViewBottomMargin))
+      .constraint(equalTo: innerView.leadingAnchor, constant: innerViewLeadingPadding + tabIconLayersViewLeadingMargin)
     let tabIconDocumentationViewTopAnchorConstraint = tabIconDocumentationView
       .topAnchor
       .constraint(
@@ -93,7 +143,10 @@ public class WorkspaceVerticalTabs: NSBox {
         constant: tabIconLayersViewBottomMargin + tabIconDocumentationViewTopMargin)
     let tabIconDocumentationViewLeadingAnchorConstraint = tabIconDocumentationView
       .leadingAnchor
-      .constraint(equalTo: leadingAnchor, constant: leadingPadding + tabIconDocumentationViewLeadingMargin)
+      .constraint(
+        equalTo: innerView.leadingAnchor,
+        constant: innerViewLeadingPadding + tabIconDocumentationViewLeadingMargin)
+    let dividerViewWidthAnchorConstraint = dividerView.widthAnchor.constraint(equalToConstant: 1)
     let tabIconLayersViewHeightAnchorConstraint = tabIconLayersView.heightAnchor.constraint(equalToConstant: 60)
     let tabIconLayersViewWidthAnchorConstraint = tabIconLayersView.widthAnchor.constraint(equalToConstant: 60)
     let tabIconDocumentationViewHeightAnchorConstraint = tabIconDocumentationView
@@ -104,33 +157,54 @@ public class WorkspaceVerticalTabs: NSBox {
       .constraint(equalToConstant: 60)
 
     NSLayoutConstraint.activate([
+      innerViewLeadingAnchorConstraint,
+      innerViewTopAnchorConstraint,
+      innerViewBottomAnchorConstraint,
+      dividerViewTrailingAnchorConstraint,
+      dividerViewLeadingAnchorConstraint,
+      dividerViewTopAnchorConstraint,
+      dividerViewBottomAnchorConstraint,
       tabIconLayersViewTopAnchorConstraint,
       tabIconLayersViewLeadingAnchorConstraint,
-      tabIconDocumentationViewBottomAnchorConstraint,
       tabIconDocumentationViewTopAnchorConstraint,
       tabIconDocumentationViewLeadingAnchorConstraint,
+      dividerViewWidthAnchorConstraint,
       tabIconLayersViewHeightAnchorConstraint,
       tabIconLayersViewWidthAnchorConstraint,
       tabIconDocumentationViewHeightAnchorConstraint,
       tabIconDocumentationViewWidthAnchorConstraint
     ])
 
+    self.innerViewLeadingAnchorConstraint = innerViewLeadingAnchorConstraint
+    self.innerViewTopAnchorConstraint = innerViewTopAnchorConstraint
+    self.innerViewBottomAnchorConstraint = innerViewBottomAnchorConstraint
+    self.dividerViewTrailingAnchorConstraint = dividerViewTrailingAnchorConstraint
+    self.dividerViewLeadingAnchorConstraint = dividerViewLeadingAnchorConstraint
+    self.dividerViewTopAnchorConstraint = dividerViewTopAnchorConstraint
+    self.dividerViewBottomAnchorConstraint = dividerViewBottomAnchorConstraint
     self.tabIconLayersViewTopAnchorConstraint = tabIconLayersViewTopAnchorConstraint
     self.tabIconLayersViewLeadingAnchorConstraint = tabIconLayersViewLeadingAnchorConstraint
-    self.tabIconDocumentationViewBottomAnchorConstraint = tabIconDocumentationViewBottomAnchorConstraint
     self.tabIconDocumentationViewTopAnchorConstraint = tabIconDocumentationViewTopAnchorConstraint
     self.tabIconDocumentationViewLeadingAnchorConstraint = tabIconDocumentationViewLeadingAnchorConstraint
+    self.dividerViewWidthAnchorConstraint = dividerViewWidthAnchorConstraint
     self.tabIconLayersViewHeightAnchorConstraint = tabIconLayersViewHeightAnchorConstraint
     self.tabIconLayersViewWidthAnchorConstraint = tabIconLayersViewWidthAnchorConstraint
     self.tabIconDocumentationViewHeightAnchorConstraint = tabIconDocumentationViewHeightAnchorConstraint
     self.tabIconDocumentationViewWidthAnchorConstraint = tabIconDocumentationViewWidthAnchorConstraint
 
     // For debugging
+    innerViewLeadingAnchorConstraint.identifier = "innerViewLeadingAnchorConstraint"
+    innerViewTopAnchorConstraint.identifier = "innerViewTopAnchorConstraint"
+    innerViewBottomAnchorConstraint.identifier = "innerViewBottomAnchorConstraint"
+    dividerViewTrailingAnchorConstraint.identifier = "dividerViewTrailingAnchorConstraint"
+    dividerViewLeadingAnchorConstraint.identifier = "dividerViewLeadingAnchorConstraint"
+    dividerViewTopAnchorConstraint.identifier = "dividerViewTopAnchorConstraint"
+    dividerViewBottomAnchorConstraint.identifier = "dividerViewBottomAnchorConstraint"
     tabIconLayersViewTopAnchorConstraint.identifier = "tabIconLayersViewTopAnchorConstraint"
     tabIconLayersViewLeadingAnchorConstraint.identifier = "tabIconLayersViewLeadingAnchorConstraint"
-    tabIconDocumentationViewBottomAnchorConstraint.identifier = "tabIconDocumentationViewBottomAnchorConstraint"
     tabIconDocumentationViewTopAnchorConstraint.identifier = "tabIconDocumentationViewTopAnchorConstraint"
     tabIconDocumentationViewLeadingAnchorConstraint.identifier = "tabIconDocumentationViewLeadingAnchorConstraint"
+    dividerViewWidthAnchorConstraint.identifier = "dividerViewWidthAnchorConstraint"
     tabIconLayersViewHeightAnchorConstraint.identifier = "tabIconLayersViewHeightAnchorConstraint"
     tabIconLayersViewWidthAnchorConstraint.identifier = "tabIconLayersViewWidthAnchorConstraint"
     tabIconDocumentationViewHeightAnchorConstraint.identifier = "tabIconDocumentationViewHeightAnchorConstraint"
