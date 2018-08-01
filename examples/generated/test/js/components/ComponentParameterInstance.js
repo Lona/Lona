71456ce1f17cd8000260418ffefd05364d5d3a18
Devin Abbott <devinabbott@gmail.com>
2018-08-01 09:23:19 -0700

    Add missing js snapshot file

---
+++ examples/generated/test/js/components/ComponentParameterInstance.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { View, StyleSheet } from "react-native"
+
+import colors from "../colors"
+import textStyles from "../textStyles"
+import ComponentParameterTemplate from "./ComponentParameterTemplate"
+
+export default class ComponentParameterInstance extends React.Component {
+  render() {
+    return (
+      <View style={[ styles.view, {} ]}>
+        <ComponentParameterTemplate
+          style={[ styles.componentParameterTemplate, {} ]}
+          subtitleComponent={{"parameters":{"text":"Subtitle","textStyle":"subheading2"},"type":"Lona:Text"}}
+          titleComponent={{"parameters":{"text":"Title","textStyle":"headline"},"type":"Lona:Text"}}
+        >
+
+        </ComponentParameterTemplate>
+      </View>
+    );
+  }
+};
+
+let styles = StyleSheet.create({
+  view: { alignSelf: "stretch" },
+  componentParameterTemplate: {}
+})
