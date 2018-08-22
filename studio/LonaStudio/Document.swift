2dd2a2f6de577f816e2de7ebcec5c92be757af20
Devin Abbott <devinabbott@gmail.com>
2018-08-22 11:05:20 -0700

    Misc document cleanup

--- studio/LonaStudio/Document.swift
+++ studio/LonaStudio/Document.swift
@@ -23,7 +23,6 @@ class Document: NSDocument {
     var name: String = "Component"
     var data: CSComponent?
     var file: CSComponent?
-//    var wc: NSWindowController? = nil
 
     var viewController: ViewController? {
         return windowControllers[0].contentViewController as? ViewController
@@ -39,34 +38,28 @@ class Document: NSDocument {
         viewController.fileURL = fileURL
     }
 
-    override func makeWindowControllers() {
-        // Returns the Storyboard that contains your Document window.
-        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
-        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier(rawValue: "Document Window Controller")) as! NSWindowController
-        if data != nil {
-            let viewController = windowController.contentViewController as! ViewController
-            viewController.setComponent(component: data!)
-            viewController.fileURL = fileURL
-        }
+    private let windowControllerId = NSStoryboard.SceneIdentifier(rawValue: "Document Window Controller")
+    private let storyboardName = NSStoryboard.Name(rawValue: "Main")
 
-        if let file = file {
-            let viewController = windowController.contentViewController as! ViewController
-            viewController.setComponent(component: file)
-            viewController.fileURL = fileURL
-        }
+    override func makeWindowControllers() {
+        let storyboard = NSStoryboard(name: storyboardName, bundle: nil)
+
+        let windowController = storyboard.instantiateController(withIdentifier: windowControllerId) as! NSWindowController
+        windowController.window?.tabbingMode = .preferred
 
         self.addWindowController(windowController)
 
-        if #available(OSX 10.12, *) {
-            let windows = self.windowControllers.map({ $0.window })
-            windows.forEach({ $0?.mergeAllWindows(self) })
-        } else {
-            // Fallback on earlier versions
+        if data != nil {
+            viewController?.setComponent(component: data!)
+            viewController?.fileURL = fileURL
+        }
+
+        if let file = file {
+            viewController?.setComponent(component: file)
+            viewController?.fileURL = fileURL
         }
 
         controller = windowController
-
-        Swift.print("Created window controller", windowController)
     }
 
     override func writeSafely(to url: URL, ofType typeName: String, for saveOperation: NSDocument.SaveOperationType) throws {
@@ -91,14 +84,12 @@ class Document: NSDocument {
     }
 
     override func read(from url: URL, ofType typeName: String) throws {
-        Swift.print("url", url)
-
         name = url.deletingPathExtension().lastPathComponent
 
         do {
             let data = try Data(contentsOf: url, options: NSData.ReadingOptions())
 
-            try read(from: data, ofType: "DocumentType")
+            try read(from: data, ofType: typeName)
         } catch {
             Swift.print(error)
         }
@@ -108,11 +99,6 @@ class Document: NSDocument {
         guard let json = try? JSONSerialization.jsonObject(with: data) else { return }
 
         file = CSComponent(CSData.from(json: json))
-
-        // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
-        // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
-        // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-//        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
     }
 
     override func save(to url: URL, ofType typeName: String, for saveOperation: NSDocument.SaveOperationType, completionHandler: @escaping (Error?) -> Void) {
