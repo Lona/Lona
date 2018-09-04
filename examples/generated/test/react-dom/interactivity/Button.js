efad109a2e183362b748a4d5ed29e9c75f9a87ee
Johnathon Sanders <johnathon.sanders@target.com>
2018-09-04 14:50:07 -0500

    adding new secondary button ability

--- examples/generated/test/react-dom/interactivity/Button.js
+++ examples/generated/test/react-dom/interactivity/Button.js
@@ -16,6 +16,9 @@ export default class Button extends React.Component {
     if (View$pressed) {
       View$backgroundColor = colors.blue50
     }
+    if (this.props.secondary==={"case":"Some","data":true}) {
+      colors.lightblue100 = View$backgroundColor
+    }
     return (
       <div
         style={Object.assign(styles.view, {
