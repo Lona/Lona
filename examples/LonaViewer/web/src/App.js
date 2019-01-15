6fe70bc5b0fed7bcb547d3619a72556d72dca0ce
Devin Abbott <devinabbott@gmail.com>
2019-01-14 16:45:28 -0800

    Add new react app

---
+++ examples/LonaViewer/web/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import Button from "generated/interactivity/Button";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button />
+      </div>
+    );
+  }
+}
+
+export default App;
