130fee5c067ce6e1cda466680843196ff6fb3fe7
mathieudutour <mathieu@dutour.me>
2019-02-06 19:35:51 -0400

    copy apache license from Xi

--- studio/LonaStudio/Plugins/RPCService.swift
+++ studio/LonaStudio/Plugins/RPCService.swift
@@ -1,10 +1,16 @@
+// Copyright 2016 The xi-editor Authors.
 //
-//  RPCService.swift
-//  LonaStudio
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Mathieu Dutour on 06/02/2019.
-//  Copyright © 2019 Devin Abbott. All rights reserved.
+// https://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // most of it is taken from https://github.com/xi-editor/xi-mac/blob/master/Sources/XiEditor/RPCSending.swift
 
