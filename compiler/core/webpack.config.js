3c1d97eecb323b9ae54b2d57afbe0b03471a39e2
Devin Abbott <devinabbott@gmail.com>
2019-02-16 12:45:03 -0800

    Update compiler package, webpack, and npm module

--- compiler/core/webpack.config.js
+++ compiler/core/webpack.config.js
@@ -16,9 +16,11 @@ const copyPlugin = new CopyWebpackPlugin([
 
 module.exports = [
   {
+    mode: "production",
     entry: "./src/main.bs.js",
     output: {
-      filename: "./build/index.js"
+      path: paths.build,
+      filename: "index.js"
     },
     target: "node",
     node: {
@@ -33,28 +35,43 @@ module.exports = [
         raw: true
       }),
       copyPlugin
-    ]
-  },
-  {
-    entry: "./src/api.js",
-    output: {
-      filename: "./build/api.js",
-      library: "lonac",
-      libraryTarget: "umd"
-    },
-    resolve: {
-      extensions: [".js"],
-      alias: {
-        fs: "memfs"
-      }
-    },
-    node: {
-      module: "empty"
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        "process.env.NODE_ENV": JSON.stringify("production")
-      })
-    ]
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              plugins: ["@babel/plugin-proposal-object-rest-spread"]
+            }
+          }
+        }
+      ]
+    }
   }
+  // {
+  //   mode: "production",
+  //   entry: "./src/api.js",
+  //   output: {
+  //     filename: "./build/api.js",
+  //     library: "lonac",
+  //     libraryTarget: "umd"
+  //   },
+  //   resolve: {
+  //     extensions: [".js"],
+  //     alias: {
+  //       fs: "memfs"
+  //     }
+  //   },
+  //   node: {
+  //     module: "empty"
+  //   },
+  //   plugins: [
+  //     new webpack.DefinePlugin({
+  //       "process.env.NODE_ENV": JSON.stringify("production")
+  //     })
+  //   ]
+  // }
 ];
