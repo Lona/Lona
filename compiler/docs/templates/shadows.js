190c167aff8883c45d549cbd8745314e18b0c429
mathieudutour <mathieu@dutour.me>
2019-03-07 18:09:27 -0400

    add shadow screen

--- compiler/docs/templates/shadows.js
+++ compiler/docs/templates/shadows.js
@@ -1,14 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
 import Layout from '../src/components/Layout'
+import ComponentTitle from '../lona-workspace/components/ComponentTitle.component'
+import ShadowCard from '../lona-workspace/components/ShadowCard.component'
 
-export default function Template({ pageContext: { shadows }, location }) {
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`
+
+export default function Template({
+  pageContext: { shadows, pathInWorkspace },
+  location,
+}) {
+  pathInWorkspace = pathInWorkspace.slice(1)
+  // eslint-disable-next-line
+  const Shadows = require(`lona-workspace/${pathInWorkspace}.json`).default
+
   return (
     <Layout location={location}>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify(shadows, null, '  '),
-        }}
-      />
+      <ComponentTitle name="Shadows" intro={shadows.description} />
+      <Wrapper>
+        {shadows.shadows.map(shadow => (
+          <ShadowCard
+            key={shadow.id}
+            shadow={Shadows[shadow.id]}
+            shadowName={shadow.name}
+            shadowCode={Shadows[shadow.id].filter}
+          />
+        ))}
+      </Wrapper>
     </Layout>
   )
 }
