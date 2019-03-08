7dadb3e2f57d23ff6d9e0993e3592ff47ca790b7
mathieudutour <mathieu@dutour.me>
2019-03-08 11:56:57 -0400

    add babel register

---
+++ compiler/sketch-library/index.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+// https://gist.github.com/branneman/8048520#gistcomment-1249909
+// Add node_modules to the path, so they're resolved even when loading modules
+// from our compilerOutput directory (which is outside the root of this project)
+process.env.NODE_PATH = path.join(__dirname, 'node_modules')
+require('module').Module._initPaths()
+
+// Load generated JS files using babel. Use a custom ignore pattern to use babel
+// even when loading outside this directory.
+require('@babel/register')({
+  ignore: [/node_modules/],
+})
+
+module.exports = require('./lib')
