580d9f071472cd26b0e29022ce2affc8f332a186
mathieudutour <mathieu@dutour.me>
2019-03-08 16:14:19 -0400

    use @lona/workspace-to-sketch-library

--- studio/plugins/sketch/index.js
+++ studio/plugins/sketch/index.js
@@ -1,19 +1,32 @@
-const os = require("os");
-const path = require("path");
+const generateLibrary = require("@lona/workspace-to-sketch-library");
+const { sendRequest } = require("stdio-jsonrpc");
+const {
+  createNewSketchFile,
+  writeSketchFile,
+  generateId
+} = require("sketch-file");
+const requestUserParameters = require("./lib/request-user-parameters");
 
-// Choose a directory for the compiler to write generated JS files
-const compilerOutput = path.join(os.tmpdir(), "lona-sketch-plugin-generated");
-
-// https://gist.github.com/branneman/8048520#gistcomment-1249909
-// Add node_modules to the path, so they're resolved even when loading modules
-// from our compilerOutput directory (which is outside the root of this project)
-process.env["NODE_PATH"] = path.join(__dirname, "node_modules");
-require("module").Module._initPaths();
-
-// Load generated JS files using babel. Use a custom ignore pattern to use babel
-// even when loading outside this directory.
-require("@babel/register")({
-  ignore: [/node_modules/]
-});
-
-require("./main")(compilerOutput);
+Promise.all([
+  sendRequest("workspacePath"),
+  sendRequest("compilerPath"),
+  sendRequest("devicePresetList"),
+  requestUserParameters()
+])
+  .then(
+    ([
+      workspace,
+      compiler,
+      devicePresetList,
+      { sketchFilePath, componentPathFilter }
+    ]) => {
+      return generateLibrary(workspace, sketchFilePath, {
+        devicePresetList,
+        compiler,
+        componentPathFilter,
+        logFunction: console.error.bind(console)
+      });
+    }
+  )
+  .catch(x => console.error(x))
+  .then(() => process.exit(0));
--- studio/plugins/sketch/lib/display-name.js
+++
@@ -1,6 +0,0 @@
-let id = 0;
-const nextId = () => ++id; // eslint-disable-line
-
-module.exports = function displayName(Component) {
-  return Component.displayName || Component.name || `UnknownSymbol${nextId()}`;
-};
