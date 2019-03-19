7f10c3981f78d62d59b746a9f5396c59709476b9
mathieudutour <mathieu@dutour.me>
2019-03-18 20:12:54 -0400

    memoize config

--- compiler/docs/tasks/load-config.js
+++ compiler/docs/tasks/load-config.js
@@ -1,7 +1,12 @@
 const path = require('path')
 const { execSync } = require('child_process')
 
+let config = null
+
 module.exports = () => {
+  if (config) {
+    return config
+  }
   const configFilePath = path.join(
     process.env.WORKSPACE ? process.env.WORKSPACE : process.cwd(),
     'lona.json'
@@ -9,7 +14,7 @@ module.exports = () => {
 
   const compilerPath = require.resolve('lonac/build/index.js')
 
-  const config = JSON.parse(
+  config = JSON.parse(
     execSync(`${compilerPath} config ${configFilePath}}`, { encoding: 'utf8' })
   )
 
