6c16117646791b89466b4dc413afba0915b504bb
Devin Abbott <devinabbott@gmail.com>
2019-06-04 15:23:28 -0700

    Use standard Logic suggestions

--- studio/LonaStudio/Logic/Views/LabeledInputs/LabeledColorInput.swift
+++ studio/LonaStudio/Logic/Views/LabeledInputs/LabeledColorInput.swift
@@ -52,16 +52,125 @@ public class LabeledColorInput: LabeledInput {
 
     private func setUpConstraints() {}
 
+    private func evaluateExpression(node: LGCSyntaxNode) -> LogicValue? {
+        guard case .expression(let expression) = node else { return nil }
+
+        let program: LGCSyntaxNode = .program(LabeledColorInput.makeExpressionProgram(from: expression).expandImports(importLoader: Library.load))
+        let scopeContext = Compiler.scopeContext(program)
+        let unificationContext = Compiler.makeUnificationContext(program, scopeContext: scopeContext)
+
+        guard case .success(let substitution) = Unification.unify(constraints: unificationContext.constraints) else {
+            return nil
+        }
+
+        let result = Compiler.evaluate(program, rootNode: program, scopeContext: scopeContext, unificationContext: unificationContext, substitution: substitution, context: .init())
+
+        switch result {
+        case .success(let evaluationContext):
+            let value = evaluationContext.values[expression.uuid]
+            return value
+        case .failure(let error):
+            Swift.print("Eval failure", error)
+            return nil
+        }
+    }
+
+    private static func makeTypeAnnotation(type: Unification.T) -> LGCTypeAnnotation {
+        switch type {
+        case .cons(name: let name, parameters: let parameters):
+            return LGCTypeAnnotation.typeIdentifier(
+                id: UUID(),
+                identifier: LGCIdentifier(id: UUID(), string: name),
+                genericArguments: .init(parameters.map(makeTypeAnnotation(type:)))
+            )
+        case .evar(let name), .gen(let name):
+            return LGCTypeAnnotation.typeIdentifier(
+                id: UUID(),
+                identifier: LGCIdentifier(id: UUID(), string: name),
+                genericArguments: .empty
+            )
+        case .fun:
+            fatalError("Not supported")
+        }
+    }
+
+    private static func makeExpressionProgram(from expression: LGCExpression) -> LGCProgram {
+        return .init(
+            id: UUID(),
+            block: .init(
+                [
+                    .declaration(
+                        id: UUID(),
+                        content: .importDeclaration(id: UUID(), name: .init(id: UUID(), name: "Prelude"))
+                    ),
+                    .declaration(
+                        id: UUID(),
+                        content: .variable(
+                            id: UUID(),
+                            name: .init(id: UUID(), name: "color"),
+                            annotation: makeTypeAnnotation(type: Unification.T.cons(name: "Optional", parameters: [.cssColor])),
+                            initializer: .some(expression)
+                        )
+                    )
+                ]
+            )
+        )
+    }
+
     private func update() {
         logicValueInput.rootNode = .expression(LogicInput.expression(forColorString: colorString))
 
+        Swift.print("Current node", logicValueInput.rootNode)
+
+        let currentValue = evaluateExpression(node: logicValueInput.rootNode)
+        Swift.print("Current value", currentValue?.memory, currentValue?.type)
+
         logicValueInput.onChangeRootNode = { [unowned self] node in
-            self.onChangeColorString?(LogicInput.makeColorString(node: node))
+            Swift.print("Change root", node)
+
+//            self.onChangeColorString?(LogicInput.makeColorString(node: node))
+
+            guard case .expression(let expression) = node else { return true }
+
+            let program: LGCSyntaxNode = .program(LabeledColorInput.makeExpressionProgram(from: expression).expandImports(importLoader: Library.load))
+
+            let scopeContext = Compiler.scopeContext(program)
+            let unificationContext = Compiler.makeUnificationContext(program, scopeContext: scopeContext)
+
+            guard case .success(let substitution) = Unification.unify(constraints: unificationContext.constraints) else {
+                return true
+            }
+
+            let result = Compiler.evaluate(program, rootNode: program, scopeContext: scopeContext, unificationContext: unificationContext, substitution: substitution, context: .init())
+
+            Swift.print(result)
+
+            switch result {
+            case .success(let evaluationContext):
+                Swift.print("Result value", evaluationContext.values[expression.uuid])
+
+                guard let value = evaluationContext.values[expression.uuid] else { break }
+
+                let colorString = LogicValue.unwrapOptional(value)?.colorString
+
+                Swift.print("Result colorString", colorString)
+
+                self.onChangeColorString?(colorString)
+            case .failure(let error):
+                Swift.print("Eval failure", error)
+            }
+
             return true
         }
 
         logicValueInput.suggestionsForNode = { rootNode, node, query in
-            return LogicInput.suggestionsForColor(isOptional: true, node: node, query: query)
+            guard case .expression(let expression) = node else { return [] }
+
+            let program: LGCSyntaxNode = .program(LabeledColorInput.makeExpressionProgram(from: expression).expandImports(importLoader: Library.load))
+
+            return StandardConfiguration.suggestions(rootNode: program, node: node, query: query) ?? []
+
+//            return LogicInput.suggestionsForColor(isOptional: true, node: node, query: query)
         }
     }
 }
