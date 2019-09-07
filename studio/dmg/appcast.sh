e865283cda59fa8ed8854e8d013ff12b894f7d6c
Devin Abbott <devinabbott@gmail.com>
2019-09-06 18:48:26 -0700

    Update release scripts

--- studio/dmg/appcast.sh
+++ studio/dmg/appcast.sh
@@ -1,38 +1,43 @@
+#!/bin/bash
+
+DIR=$(dirname "$0")
+
+pushd "$DIR"/../build
+
+if [[ -z "${LONASTUDIO_DSA_PRIVATE_KEY}" ]]; then
+	echo "You must configure the DSA private key: LONASTUDIO_DSA_PRIVATE_KEY"
+	exit 1
+fi
+
+if [[ -z "${LONASTUDIO_APPCAST_DIRECTORY}" ]]; then
+	echo "You must set the path to the appcast directory: LONASTUDIO_APPCAST_DIRECTORY"
+	exit 1
+fi
+
 if ! [ -e LonaStudio.app ]; then
 	echo "Put the archived app, LonaStudio.app, in this directory to update the appcast."
 	exit 1
 fi
 
-if ! [ -e LonaStudio.zip ]; then
-	echo "You must first build a zip in order to appcast."
-	exit 1
-fi
-
-if [[ -z "${COMPONENT_STUDIO_DSA_PRIVATE_KEY}" ]]; then
-	echo "You must set the path to the private key: COMPONENT_STUDIO_DSA_PRIVATE_KEY"
-  	exit 1
-fi
-
-if [[ -z "${COMPONENT_STUDIO_APPCAST_DIRECTORY}" ]]; then
-	echo "You must set the path to the appcast directory: COMPONENT_STUDIO_APPCAST_DIRECTORY"
-  	exit 1
-fi
+../dmg/build-dmg.sh
 
 VERSION=$(mdls -name kMDItemVersion LonaStudio.app | awk -F'"' '{print $2}')
-TARGET="$COMPONENT_STUDIO_APPCAST_DIRECTORY"LonaStudio_"$VERSION".zip
+TARGET="$LONASTUDIO_APPCAST_DIRECTORY"/LonaStudio_"$VERSION".dmg
 
-echo "Copying .zip to $TARGET"
+echo "Copying .dmg to $TARGET"
 
-cp LonaStudio.zip "$TARGET"
+cp LonaStudio.dmg "$TARGET"
 
 echo "Generating appcast"
 
-../Pods/Sparkle/bin/generate_appcast "$COMPONENT_STUDIO_DSA_PRIVATE_KEY" "$COMPONENT_STUDIO_APPCAST_DIRECTORY"
+../tools/sparkle/bin/generate_appcast "$LONASTUDIO_DSA_PRIVATE_KEY" "$LONASTUDIO_APPCAST_DIRECTORY"
 
 echo "Generated appcast!"
 echo ""
 
-cat "$COMPONENT_STUDIO_APPCAST_DIRECTORY"appcast.xml
+cat "$LONASTUDIO_APPCAST_DIRECTORY"/appcast.xml
 
 echo ""
 echo ""
+
+popd
--- studio/dmg/build-dmg.sh
+++ studio/dmg/build-dmg.sh
@@ -1,12 +1,18 @@
+DIR=$(dirname "$0")
+
+pushd "$DIR"/../build
+
 [ -e LonaStudio.dmg ] && rm LonaStudio.dmg
 
 if ! [ -e LonaStudio.app ]; then
-	echo "Put the archived app, LonaStudio.app, in this directory to build a dmg."
+	echo "Put the archived app, LonaStudio.app, in the ../build directory to build a zip."
 	exit 1
 fi
 
 # npm install -g appdmg
-appdmg appdmg.json LonaStudio.dmg
+appdmg ../dmg/appdmg.json LonaStudio.dmg
 
 # https://stackoverflow.com/questions/23824815/how-to-add-codesigning-to-dmg-file-in-mac
 codesign -s "Developer ID Application: Devin Abbott (CV2RHZWPY9)" LonaStudio.dmg
+
+popd
