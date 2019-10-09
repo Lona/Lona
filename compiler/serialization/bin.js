191e1081d0a598f80d62d74fec8e26583df0490b
Devin Abbott <devinabbott@gmail.com>
2019-10-09 14:12:43 -0700

    Add yargs for improved cli usage

--- compiler/serialization/bin.js
+++ compiler/serialization/bin.js
@@ -1,71 +1,62 @@
 #!/usr/bin/env node
 
+const path = require('path')
 const fs = require('fs')
+const yargs = require('yargs')
+
 const { convertTypes, convertLogic, convertDocument } = require('./lib/index')
 
-const [, , filename, kind, targetFormat] = process.argv
-
-const usage = 'usage: filename kind targetFormat'
-
-if (!filename) {
-  console.log('No filename')
-  console.log(usage)
-  process.exit(1)
+function addSharedArguments(yargs) {
+  yargs.positional('file', {
+    type: 'string',
+    describe: 'The file to convert',
+  })
+  yargs.positional('targetFormat', {
+    type: 'string',
+    describe: 'The target format',
+  })
 }
 
-if (kind !== 'types' && kind !== 'logic' && kind !== 'document') {
-  console.log(
-    'Only type, logic, and document files support conversion currently'
+yargs
+  .scriptName('@lona/serialization')
+  .usage('Usage: @lona/serialization <command> [options]')
+  .command(
+    'document file targetFormat',
+    'Convert a Lona document to the specified format',
+    yargs => addSharedArguments(yargs),
+    argv => {
+      const { file, targetFormat, sourceFormat } = argv
+      const contents = fs.readFileSync(file, 'utf8')
+      const converted = convertDocument(contents, targetFormat)
+      console.log(converted)
+    }
   )
-  console.log(usage)
-  process.exit(1)
-}
-
-if (!targetFormat) {
-  console.log('No serialization targetFormat')
-  console.log(usage)
-  process.exit(1)
-}
-
-function convertTypesFile(file, targetFormat) {
-  const contents = fs.readFileSync(filename, 'utf8')
-
-  return convertTypes(contents, targetFormat)
-}
-
-function convertLogicFile(file, targetFormat) {
-  const contents = fs.readFileSync(filename, 'utf8')
-
-  return convertLogic(contents, targetFormat)
-}
-
-function convertDocumentFile(file, targetFormat) {
-  const contents = fs.readFileSync(filename, 'utf8')
-
-  return convertDocument(contents, targetFormat)
-}
-
-try {
-  switch (kind) {
-    case 'types': {
-      const convertedString = convertTypesFile(filename, targetFormat)
-      console.log(convertedString)
-      break
+  .command(
+    'logic file targetFormat',
+    'Convert a Lona logic (tokens) file to the specified format',
+    yargs => addSharedArguments(yargs),
+    argv => {
+      const { file, targetFormat, sourceFormat } = argv
+      const contents = fs.readFileSync(file, 'utf8')
+      const converted = convertLogic(contents, targetFormat)
+      console.log(converted)
     }
-    case 'logic': {
-      const convertedString = convertLogicFile(filename, targetFormat)
-      console.log(convertedString)
-      break
+  )
+  .command(
+    'types file targetFormat',
+    'Convert a Lona types file to the specified format',
+    yargs => addSharedArguments(yargs),
+    argv => {
+      const { file, targetFormat, sourceFormat } = argv
+      const contents = fs.readFileSync(file, 'utf8')
+      const converted = convertTypes(contents, targetFormat)
+      console.log(converted)
     }
-    case 'document': {
-      const convertedString = convertDocumentFile(filename, targetFormat)
-      console.log(convertedString)
-      break
-    }
-    default:
-      console.log('Unknown kind of file')
-  }
-} catch (e) {
-  console.error(e)
-  process.exit(1)
-}
+  )
+  .demandCommand(1, '\nPass --help to see all available commands and options.')
+  .strict()
+  .fail((msg, err, yargs) => {
+    yargs.showHelp()
+    console.log(msg)
+  })
+  .help().argv
