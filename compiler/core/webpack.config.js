1398cd01d3ab2489850428bb54bb3c7d9bdb7d5f
Devin Abbott <devinabbott@gmail.com>
2019-10-30 13:53:46 -0700

    Update lonac webpack config and bump version

--- compiler/core/webpack.config.js
+++ compiler/core/webpack.config.js
@@ -1,40 +1,48 @@
-const { resolve, join } = require("path");
-const webpack = require("webpack");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { resolve, join } = require('path')
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const paths = {
-  build: resolve(__dirname, "build"),
-  src: resolve(__dirname, "src")
-};
+  build: resolve(__dirname, 'build'),
+  src: resolve(__dirname, 'src'),
+  nodeModules: resolve(__dirname, 'node_modules'),
+}
 
 const copyPlugin = new CopyWebpackPlugin([
   {
-    from: join(paths.src, "static"),
-    to: join(paths.build, "static")
-  }
-]);
+    from: join(paths.src, 'static'),
+    to: join(paths.build, 'static'),
+  },
+  {
+    from: join(
+      paths.nodeModules,
+      '@lona/serialization/build/pegjs/logic.swift.pegjs'
+    ),
+    to: join(paths.build, 'logic.swift.pegjs'),
+  },
+])
 
 module.exports = [
   {
-    mode: "production",
-    entry: "./src/main.bs.js",
+    mode: 'production',
+    entry: './src/main.bs.js',
     output: {
       path: paths.build,
-      filename: "index.js"
+      filename: 'index.js',
     },
-    target: "node",
+    target: 'node',
     node: {
-      __dirname: false
+      __dirname: false,
     },
     plugins: [
       new webpack.DefinePlugin({
-        "process.env.NODE_ENV": JSON.stringify("production")
+        'process.env.NODE_ENV': JSON.stringify('production'),
       }),
       new webpack.BannerPlugin({
-        banner: "#!/usr/bin/env node",
-        raw: true
+        banner: '#!/usr/bin/env node',
+        raw: true,
       }),
-      copyPlugin
+      copyPlugin,
     ],
     module: {
       rules: [
@@ -42,15 +50,15 @@ module.exports = [
           test: /\.m?js$/,
           exclude: /(node_modules)/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              plugins: ["@babel/plugin-proposal-object-rest-spread"]
-            }
-          }
-        }
-      ]
-    }
-  }
+              plugins: ['@babel/plugin-proposal-object-rest-spread'],
+            },
+          },
+        },
+      ],
+    },
+  },
   // {
   //   mode: "production",
   //   entry: "./src/api.js",
@@ -74,4 +82,4 @@ module.exports = [
   //     })
   //   ]
   // }
-];
+]
