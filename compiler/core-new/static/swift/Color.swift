79b7d445979290350036ad5836df86e1bfa04e29
Mathieu Dutour <mathieu@dutour.me>
2020-01-26 22:00:50 +0100

    no public typealias

--- compiler/core-new/static/swift/Color.swift
+++ compiler/core-new/static/swift/Color.swift
@@ -2,8 +2,8 @@ import Foundation
 
 #if os(iOS) || os(tvOS) || os(watchOS)
   import UIKit
-  public typealias Image = UIColor
+  typealias Image = UIColor
 #elseif os(macOS)
   import Cocoa
-  public typealias Image = NSColor
+  typealias Image = NSColor
 #endif
--- compiler/core-new/static/swift/Image.swift
+++ compiler/core-new/static/swift/Image.swift
@@ -2,8 +2,8 @@ import Foundation
 
 #if os(iOS) || os(tvOS) || os(watchOS)
   import UIKit
-  public typealias Image = UIImage
+  typealias Image = UIImage
 #elseif os(macOS)
   import Cocoa
-  public typealias Image = NSImage
+  typealias Image = NSImage
 #endif
