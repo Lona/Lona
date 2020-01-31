c5497e3756a144f1ee81aa6068e3352fc3154b66
Mathieu Dutour <mathieu@dutour.me>
2020-01-31 15:50:41 +0100

    generate hardcoding map from prelude

---
+++ compiler/core-new/scripts/generate-prelude-types.ts
@@ -0,0 +1,121 @@
+import * as fs from 'fs'
+import * as path from 'path'
+
+import * as LogicAST from '../src/helpers/logic-ast'
+import { build } from '../src/helpers/logic-scope'
+
+const preludePath = path.join(__dirname, '../static/logic')
+const preludeLibs = fs.readdirSync(preludePath)
+
+const libraryFiles: LogicAST.AST.Program[] = preludeLibs.map(
+  x =>
+    LogicAST.makeProgram(
+      JSON.parse(fs.readFileSync(path.join(preludePath, x), 'utf8'))
+    ) as LogicAST.AST.Program
+)
+
+const preludeProgram = LogicAST.joinPrograms(libraryFiles)
+
+const scopeContext = build(preludeProgram)
+
+const hardcodedMapping = scopeContext._namespace.map
+  .map<[string[], LogicAST.AST.SyntaxNode | void]>(x => [
+    x.key,
+    LogicAST.getNode(preludeProgram, x.value),
+  ])
+  .reduce(
+    (prev, [key, node]) => {
+      if (!node) {
+        return prev
+      }
+
+      if (node.type === 'function' || node.type === 'record') {
+        prev.functionCallExpression[key.join('.')] = node
+      }
+      if (node.type === 'variable') {
+        prev.memberExpression[key.join('.')] = node
+      }
+
+      if (node.type === 'enumerationCase') {
+        if (
+          node.data.associatedValueTypes.some(x => x.type !== 'placeholder')
+        ) {
+          prev.functionCallExpression[key.join('.')] = node
+        } else {
+          prev.memberExpression[key.join('.')] = node
+        }
+      }
+
+      return prev
+    },
+    { functionCallExpression: {}, memberExpression: {} }
+  )
+
+const syntaxNodeMapping = {
+  functionCallExpression: 'FunctionCallExpressionExpression',
+  memberExpression: 'MemberExpressionExpression',
+}
+
+fs.writeFileSync(
+  path.join(__dirname, '../src/helpers/hardcoded-mapping.ts'),
+  `// ⚠️ THIS FILE IS AUTO GENERATED. DO NOT MODIFY IT.
+
+import { AST as LogicAST, flattenedMemberExpression } from './logic-ast'
+import { EvaluationContext } from './logic-evaluate'
+
+export type HardcodedMap<T> = {
+${Object.keys(hardcodedMapping)
+  .map(
+    k => `  ${k}: {
+${Object.keys(hardcodedMapping[k])
+  .map(
+    x =>
+      `    '${x}': (
+      node: LogicAST.${syntaxNodeMapping[k]},
+      ...args: any[]
+    ) => T | void`
+  )
+  .join('\n')}
+  }`
+  )
+  .join('\n')}
+}
+
+export const HandlePreludeFactory = <T>(hardcodedMap: HardcodedMap<T>) => (
+  node: LogicAST.SyntaxNode,
+  evaluationContext: void | EvaluationContext,
+  ...args: any[]
+): T | void => {
+  if (!evaluationContext) {
+    return
+  }
+
+  let matchedHardcodedNode: T | void
+
+  Object.keys(hardcodedMap).forEach(
+    (x: ${Object.keys(hardcodedMapping)
+      .map(k => `'${k}'`)
+      .join(' | ')}) => {
+      if (node.type === x) {
+        let memberExpression =
+          'memberName' in node.data ? node : node.data.expression
+
+        if (!evaluationContext.isFromInitialScope(memberExpression.data.id)) {
+          return
+        }
+
+        const path = (flattenedMemberExpression(memberExpression) || [])
+          .map(y => y.string)
+          .join('.')
+
+        if (hardcodedMap[x][path]) {
+          matchedHardcodedNode = hardcodedMap[x][path](node, ...args)
+        }
+      }
+    }
+  )
+
+  return matchedHardcodedNode
+}
+`
+)
---
+++ compiler/core-new/src/helpers/hardcoded-mapping.ts
@@ -0,0 +1,154 @@
+// ⚠️ THIS FILE IS AUTO GENERATED. DO NOT MODIFY IT.
+
+import { AST as LogicAST, flattenedMemberExpression } from './logic-ast'
+import { EvaluationContext } from './logic-evaluate'
+
+export type HardcodedMap<T> = {
+  functionCallExpression: {
+    'Color.saturate': (
+      node: LogicAST.FunctionCallExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'Boolean.or': (
+      node: LogicAST.FunctionCallExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'Boolean.and': (
+      node: LogicAST.FunctionCallExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'String.concat': (
+      node: LogicAST.FunctionCallExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'Optional.value': (
+      node: LogicAST.FunctionCallExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'Shadow': (
+      node: LogicAST.FunctionCallExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'TextStyle': (
+      node: LogicAST.FunctionCallExpressionExpression,
+      ...args: any[]
+    ) => T | void
+  }
+  memberExpression: {
+    'Optional.none': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.ultraLight': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.thin': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.light': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.regular': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.medium': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.semibold': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.bold': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.heavy': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.black': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.w100': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.w200': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.w300': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.w400': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.w500': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.w600': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.w700': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.w800': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'FontWeight.w900': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+    'TextStyle.systemTextColor': (
+      node: LogicAST.MemberExpressionExpression,
+      ...args: any[]
+    ) => T | void
+  }
+}
+
+export const HandlePreludeFactory = <T>(hardcodedMap: HardcodedMap<T>) => (
+  node: LogicAST.SyntaxNode,
+  evaluationContext: void | EvaluationContext,
+  ...args: any[]
+): T | void => {
+  if (!evaluationContext) {
+    return
+  }
+
+  let matchedHardcodedNode: T | void
+
+  Object.keys(hardcodedMap).forEach(
+    (x: 'functionCallExpression' | 'memberExpression') => {
+      if (node.type === x) {
+        let memberExpression =
+          'memberName' in node.data ? node : node.data.expression
+
+        if (!evaluationContext.isFromInitialScope(memberExpression.data.id)) {
+          return
+        }
+
+        const path = (flattenedMemberExpression(memberExpression) || [])
+          .map(y => y.string)
+          .join('.')
+
+        if (hardcodedMap[x][path]) {
+          matchedHardcodedNode = hardcodedMap[x][path](node, ...args)
+        }
+      }
+    }
+  )
+
+  return matchedHardcodedNode
+}
