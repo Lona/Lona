3a4f4ff611a26d3db120eb56aef3dd1eb72ace61
Devin Abbott <devinabbott@gmail.com>
2020-02-11 10:26:27 -0800

    Update landing page button style

---
+++ studio/LonaStudio/Generated/components/PrimaryButton.swift
@@ -0,0 +1,229 @@
+import AppKit
+import Foundation
+
+// MARK: - PrimaryButton
+
+public class PrimaryButton: NSBox {
+
+  // MARK: Lifecycle
+
+  public init(_ parameters: Parameters) {
+    self.parameters = parameters
+
+    super.init(frame: .zero)
+
+    setUpViews()
+    setUpConstraints()
+
+    update()
+
+    addTrackingArea(trackingArea)
+  }
+
+  public convenience init(titleText: String) {
+    self.init(Parameters(titleText: titleText))
+  }
+
+  public convenience init() {
+    self.init(Parameters())
+  }
+
+  public required init?(coder aDecoder: NSCoder) {
+    self.parameters = Parameters()
+
+    super.init(coder: aDecoder)
+
+    setUpViews()
+    setUpConstraints()
+
+    update()
+
+    addTrackingArea(trackingArea)
+  }
+
+  deinit {
+    removeTrackingArea(trackingArea)
+  }
+
+  // MARK: Public
+
+  public var onClick: (() -> Void)? {
+    get { return parameters.onClick }
+    set { parameters.onClick = newValue }
+  }
+
+  public var titleText: String {
+    get { return parameters.titleText }
+    set {
+      if parameters.titleText != newValue {
+        parameters.titleText = newValue
+      }
+    }
+  }
+
+  public var parameters: Parameters {
+    didSet {
+      if parameters != oldValue {
+        update()
+      }
+    }
+  }
+
+  // MARK: Private
+
+  private lazy var trackingArea = NSTrackingArea(
+    rect: self.frame,
+    options: [.mouseEnteredAndExited, .activeAlways, .mouseMoved, .inVisibleRect],
+    owner: self)
+
+  private var titleView = LNATextField(labelWithString: "")
+
+  private var titleViewTextStyle = TextStyles.regular.with(alignment: .center)
+
+  private var hovered = false
+  private var pressed = false
+  private var onPress: (() -> Void)?
+
+  private func setUpViews() {
+    boxType = .custom
+    borderType = .lineBorder
+    contentViewMargins = .zero
+    titleView.lineBreakMode = .byWordWrapping
+
+    addSubview(titleView)
+
+    borderColor = Colors.darkTransparentOutline
+    cornerRadius = 2
+    borderWidth = 1
+    titleView.maximumNumberOfLines = 1
+  }
+
+  private func setUpConstraints() {
+    translatesAutoresizingMaskIntoConstraints = false
+    titleView.translatesAutoresizingMaskIntoConstraints = false
+
+    let titleViewTopAnchorConstraint = titleView.topAnchor.constraint(equalTo: topAnchor, constant: 9)
+    let titleViewBottomAnchorConstraint = titleView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -9)
+    let titleViewLeadingAnchorConstraint = titleView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 9)
+    let titleViewTrailingAnchorConstraint = titleView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -9)
+
+    NSLayoutConstraint.activate([
+      titleViewTopAnchorConstraint,
+      titleViewBottomAnchorConstraint,
+      titleViewLeadingAnchorConstraint,
+      titleViewTrailingAnchorConstraint
+    ])
+  }
+
+  private func update() {
+    fillColor = Colors.white
+    titleView.attributedStringValue = titleViewTextStyle.apply(to: titleText)
+    onPress = handleOnClick
+    if hovered {
+      fillColor = Colors.darkTransparentOutline
+    }
+  }
+
+  private func handleOnClick() {
+    onClick?()
+  }
+
+  private func updateHoverState(with event: NSEvent) {
+    let hovered = bounds.contains(convert(event.locationInWindow, from: nil))
+    if hovered != self.hovered {
+      self.hovered = hovered
+
+      update()
+    }
+  }
+
+  public override func mouseEntered(with event: NSEvent) {
+    updateHoverState(with: event)
+  }
+
+  public override func mouseMoved(with event: NSEvent) {
+    updateHoverState(with: event)
+  }
+
+  public override func mouseDragged(with event: NSEvent) {
+    updateHoverState(with: event)
+  }
+
+  public override func mouseExited(with event: NSEvent) {
+    updateHoverState(with: event)
+  }
+
+  public override func mouseDown(with event: NSEvent) {
+    let pressed = bounds.contains(convert(event.locationInWindow, from: nil))
+    if pressed != self.pressed {
+      self.pressed = pressed
+
+      update()
+    }
+  }
+
+  public override func mouseUp(with event: NSEvent) {
+    let clicked = pressed && bounds.contains(convert(event.locationInWindow, from: nil))
+
+    if pressed {
+      pressed = false
+
+      update()
+    }
+
+    if clicked {
+      onPress?()
+    }
+  }
+}
+
+// MARK: - Parameters
+
+extension PrimaryButton {
+  public struct Parameters: Equatable {
+    public var titleText: String
+    public var onClick: (() -> Void)?
+
+    public init(titleText: String, onClick: (() -> Void)? = nil) {
+      self.titleText = titleText
+      self.onClick = onClick
+    }
+
+    public init() {
+      self.init(titleText: "")
+    }
+
+    public static func ==(lhs: Parameters, rhs: Parameters) -> Bool {
+      return lhs.titleText == rhs.titleText
+    }
+  }
+}
+
+// MARK: - Model
+
+extension PrimaryButton {
+  public struct Model: LonaViewModel, Equatable {
+    public var id: String?
+    public var parameters: Parameters
+    public var type: String {
+      return "PrimaryButton"
+    }
+
+    public init(id: String? = nil, parameters: Parameters) {
+      self.id = id
+      self.parameters = parameters
+    }
+
+    public init(_ parameters: Parameters) {
+      self.parameters = parameters
+    }
+
+    public init(titleText: String, onClick: (() -> Void)? = nil) {
+      self.init(Parameters(titleText: titleText, onClick: onClick))
+    }
+
+    public init() {
+      self.init(titleText: "")
+    }
+  }
+}
